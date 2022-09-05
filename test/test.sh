f9dfe2d69c45658ac0dc7c3264f3a806907f95ee
mlyxshi <mlyxdev@gmail.com>
2022-09-05 06:11:50 -0700

    test

--- test/test.sh
+++ test/test.sh
@@ -12,7 +12,8 @@ jq --null-input \
   --arg latestIPAUrl $latestIPAUrl  \
   --arg date $date  \
   --argjson size $size \
-  --arg whatsnew $whatsnew  \
-  '{ "name": "Bangumi Repo", "identifier": "czy0729.bangumi.AltSource", "apps": [{ "beta": false, "name": "Bangumi", "bundleIdentifier": "tv.bangumi.czy0729", "developerName": "czy0729", "subtitle": "Bangumi", "version": $latestVersion, "versionDate": $date, "versionDescription": $whatsnew, "downloadURL": $latestIPAUrl, "localizedDescription": "An unofficial https://bgm.tv app client for Android and iOS, built with React Native", "iconURL": "https://raw.githubusercontent.com/czy0729/Bangumi/master/src/assets/images/icon.png", "tintColor": "9b54fd", "size": $size, "screenshotURLs": ["https://user-images.githubusercontent.com/13514316/171143439-5e9c6c22-889d-4774-8c66-4316294d5019.png"] }] }' \
+  '{ "name": "Bangumi Repo", "identifier": "czy0729.bangumi.AltSource", "apps": [{ "beta": false, "name": "Bangumi", "bundleIdentifier": "tv.bangumi.czy0729", "developerName": "czy0729", "subtitle": "Bangumi", "version": $latestVersion, "versionDate": $date, "versionDescription": "tmp", "downloadURL": $latestIPAUrl, "localizedDescription": "An unofficial https://bgm.tv app client for Android and iOS, built with React Native", "iconURL": "https://raw.githubusercontent.com/czy0729/Bangumi/master/src/assets/images/icon.png", "tintColor": "9b54fd", "size": $size, "screenshotURLs": ["https://user-images.githubusercontent.com/13514316/171143439-5e9c6c22-889d-4774-8c66-4316294d5019.png"] }] }' \
   >  bangumi.json
+
+cat bangumi.json| jq  ".apps[0].versionDescription|=$whatsnew" > out.json
     
